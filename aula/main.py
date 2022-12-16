3eb16e960c45e9d8140ca970e14e4ecd84e52a75
Luis Cidade Câmara <105284026+luiscidade17@users.noreply.github.com>
2022-12-16 10:18:36 -0100

    Create main.py

---
+++ aula/main.py
@@ -0,0 +1,10 @@
+from myclasses import casa
+minhacasa = casa(4, "vermelho")
+minhacasa2 = casa(3, "Verde")
+minhacasa3 = casa(2, "Amarelo")
+
+print(minhacasa.cor)
+minhacasa.imprimeqq()
+print(minhacasa.idade)
+minhacasa.idade = 10
+print(minhacasa.idade)

969619e1f9fe4fc2b29f82aa03a2ca09169ec355
Luis Cidade Câmara <105284026+luiscidade17@users.noreply.github.com>
2022-12-16 10:27:23 -0100

    Create myalunos.py

---
+++ Alunos/myalunos.py
@@ -0,0 +1,9 @@
+class Aluno:
+    def __init__(self,idade,nome):
+        self.idade=idade
+        self.nome=nome
+        
+    def set_idade(self,idade):
+        self.idade=idade
+    def set_nome(self,nome):
+        self.nome=nome

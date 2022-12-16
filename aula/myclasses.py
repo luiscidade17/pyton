72f567c5ab18b4118ebc241b22a863c8f5db19d1
Luis Cidade Câmara <105284026+luiscidade17@users.noreply.github.com>
2022-12-16 10:19:08 -0100

    Create myclasses.py

---
+++ aula/myclasses.py
@@ -0,0 +1,12 @@
+class casa:
+    
+    nome="o nome"
+    idade=33
+    def __init__(self, divisoes, cor):
+        self.divisoes = divisoes
+        self.cor = cor       
+
+        
+    def imprimeqq (self):
+        print("mundo")
+        
